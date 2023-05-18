46f120827bc3a486fe5571e953e405f6825e1ceb
Inna Dmytrenko <inna.shalun@gmail.com>
2023-05-18 22:17:28 +0300

    update Modal

--- src/components/ImageGallery/ImageGalleryItem/Modal/Modal.jsx
+++ src/components/ImageGallery/ImageGalleryItem/Modal/Modal.jsx
@@ -1,38 +1,34 @@
-import { Component } from 'react';
+import {useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { ModalImg, ModalStyle, Overlay } from './Modal.styled';
-class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.clickEsc);
-    }
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.clickEsc);
-    }
+function Modal ({onClose, children, imgData}) {
+    useEffect(() => {
+        function handleClose(e){
+            if (e.code === 'Escape'){
+                onClose()
+            }
+        }
+        window.addEventListener('keydown', handleClose);
+            return () => {
+                window.removeEventListener('keydown', handleClose);
+            };
+            }, [onClose]);
 
-    clickBackdrop = event => {
+    const clickBackdrop = event => {
         if (event.target === event.currentTarget) {
-            this.props.onClose();
+            onClose();
         }
     }
 
-    clickEsc = event => {
-        if (event.code === 'Escape') {
-            this.props.onClose();
-        }
-    }
-
-
-    render() {
         return (
-            <Overlay onClick={this.clickBackdrop}>
-                <ModalStyle>{this.props.children}
-                    <ModalImg src={this.props.imgData.largeImageURL} alt={this.props.imgData.tags} />
+            <Overlay onClick={clickBackdrop}>
+                <ModalStyle>{children}
+                    <ModalImg src={imgData.largeImageURL} alt={imgData.tags} />
                 </ModalStyle>
             </Overlay>
         )
     }
-}
 
 Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
