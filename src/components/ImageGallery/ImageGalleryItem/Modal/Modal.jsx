03ce041f645fea7df01a87c997ab94aeefa2833a
Inna Dmytrenko <inna.shalun@gmail.com>
2023-05-18 22:28:44 +0300

    update Searchbar

--- src/components/ImageGallery/ImageGalleryItem/Modal/Modal.jsx
+++ src/components/ImageGallery/ImageGalleryItem/Modal/Modal.jsx
@@ -1,19 +1,19 @@
-import {useEffect } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { ModalImg, ModalStyle, Overlay } from './Modal.styled';
-function Modal ({onClose, children, imgData}) {
+function Modal({ onClose, children, imgData }) {
     useEffect(() => {
-        function handleClose(e){
-            if (e.code === 'Escape'){
+        function handleClose(e) {
+            if (e.code === 'Escape') {
                 onClose()
             }
         }
         window.addEventListener('keydown', handleClose);
-            return () => {
-                window.removeEventListener('keydown', handleClose);
-            };
-            }, [onClose]);
+        return () => {
+            window.removeEventListener('keydown', handleClose);
+        };
+    }, [onClose]);
 
     const clickBackdrop = event => {
         if (event.target === event.currentTarget) {
@@ -21,14 +21,14 @@ function Modal ({onClose, children, imgData}) {
         }
     }
 
-        return (
-            <Overlay onClick={clickBackdrop}>
-                <ModalStyle>{children}
-                    <ModalImg src={imgData.largeImageURL} alt={imgData.tags} />
-                </ModalStyle>
-            </Overlay>
-        )
-    }
+    return (
+        <Overlay onClick={clickBackdrop}>
+            <ModalStyle>{children}
+                <ModalImg src={imgData.largeImageURL} alt={imgData.tags} />
+            </ModalStyle>
+        </Overlay>
+    )
+}
 
 Modal.propTypes = {
     onClose: PropTypes.func.isRequired,
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,51 +1,46 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Header, Form, Button, Input, StyledSvg} from './Searchbar.styled';
+import { Header, Form, Button, Input, StyledSvg } from './Searchbar.styled';
 import { toast } from 'react-toastify';
 
-class Searchbar extends Component {
-  state = {
-    value: '',
-  };
+function Searchbar({ onSubmit }) {
+  const [value, setValue] = useState('');
 
-  handlerSubmit = e => {
+  const handlerSubmit = e => {
     e.preventDefault();
 
-    if (this.state.value.trim() === '') {
+    if (value.trim() === '') {
       toast("What are you searching for?");
       return;
-  };
-  this.props.onSubmit(this.state.value);
-  this.setState({ value: '' });
-}
-
-  onChange = event => {
-    this.setState({ value: event.currentTarget.value.toLowerCase() })
-}
-
-  render() {
-    const { value } = this.state;
-    return (
-      <Header>
-        <Form onSubmit={this.handlerSubmit}>
-          <Button type="submit">
-            <StyledSvg />
-          </Button>
-
-          <Input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.onChange}
-            value={value}
-          />
-        </Form>
-      </Header>
-    );
+    };
+    onSubmit(value);
+    setValue('');
   }
+
+  const onChange = event => {
+    setValue(event.currentTarget.value.toLowerCase())
+  }
+
+  return (
+    <Header>
+      <Form onSubmit={handlerSubmit}>
+        <Button type="submit">
+          <StyledSvg />
+        </Button>
+
+        <Input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onChange}
+          value={value}
+        />
+      </Form>
+    </Header>
+  );
 }
 
 Searchbar.propTypes = {
