48632e7f0d7d675f361eb04cd9ee526ed21b67c2
Inna Dmytrenko <inna.shalun@gmail.com>
2023-05-18 23:26:21 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import {useState, useEffect } from 'react';
 
 import GalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import { Item, Wrap, Text } from './ImageGallery.styled';
@@ -7,61 +7,32 @@ import Loader from './ImageGalleryItem/Loader/Loader';
 import getImages from '../services/api'
 import LoadMoreBtn from './ImageGalleryItem/Button/Button';
 
-export class ImageGallery extends Component {
-  state = {
-    imageList: [],
-    totalPage: 0,
-    error: null,
-    status: 'idle',
-    page: 1,
-    largeImageURL: '',
-    tags: '',
+function ImageGallery ({showModal, searchQuery}) {
+  const [imageList, setImageList] = useState([])
+  const [totalPage, setTotalPage] = useState(0)
+  const [error, setError] = useState(null)
+  const [status, setStatus] = useState('idle')
+  const [page, setPage] = useState(1)
+  const [largeImageURL, setLargeImageURL] = useState('')
+  const [tags, setTags] = useState('')
+
+  useEffect(() => {
+    if (searchQuery === '') return;
+    setStatus('pending');
+    getImages(searchQuery, page)
+        .then(data => {
+          setImageList((hue)=> hue.imageList.concat(data.hits));
+          setTotalPage( data.totalPage);
+          setStatus('resolved')
+        } )
+        .catch(error => {setError(error); setStatus('rejected')})
+  }, [searchQuery, page]);
+
+const handlerBtnClick = (e) => {
+    setPage(
+    page + 1,
+    );
   };
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevSearchQuery = prevProps.searchQuery;
-    const nextSearchQuery = this.props.searchQuery;
-    const prevPage = prevState.page;
-    const currentPage = this.state.page;
-
-    const isNewSearchQuery = prevSearchQuery !== nextSearchQuery;
-
-    if (isNewSearchQuery) {
-      this.setState({ imageList: [], page: 1 });
-    }
-
-    if (isNewSearchQuery || prevPage !== currentPage) {
-      const requestPage= isNewSearchQuery ? 1 : currentPage;
-
-      if (this.state.status === 'pending') return 
-
-      this.setState({ status: 'pending' });
-
-      getImages(nextSearchQuery, requestPage)
-        .then(data => this.setState((currentState) => ({
-          imageList: currentState.imageList.concat(data.hits),
-          totalPage: data.totalPage,
-          status: 'resolved'
-        })))
-        .catch(error => this.setState({ error, status: 'rejected' }))
-    }
-  }
-
-  handlerBtnClick = (e) => {
-    const { page } = this.state;
-    this.setState({
-      page: page + 1,
-    });
-  };
-
-  render() {
-    const {
-      imageList,
-      error,
-      status,
-      page,
-      totalPage
-    } = this.state;
 
     if (status === 'idle') {
       return <Text>Enter a search query</Text>;
@@ -85,14 +56,14 @@ export class ImageGallery extends Component {
               webformatURL={webformatURL}
               tags={tags}
               largeImageURL={largeImageURL}
-              openModal={() => this.props.showModal({ largeImageURL, tags })} />
+              openModal={() => showModal({ largeImageURL, tags })} />
           ))}
         </Item>
 
-        {page < totalPage && <LoadMoreBtn isLoading={status === 'pending'} onClick={this.handlerBtnClick} />}
+        {page < totalPage && <LoadMoreBtn isLoading={status === 'pending'} onClick={handlerBtnClick} />}
       </Wrap>
     );
   }
-};
+
 
 export default ImageGallery;
